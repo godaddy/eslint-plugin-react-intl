08e93b2b3415388d01ee7b2265d39d456ab6ac5e
Tony Jose Garcia Alcala <tgarciaalcala@godaddy.com>
2022-04-22 12:09:51 -0400

    feat: some comments added

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   collectCoverageFrom: ['lib/**/*.js'],
+  // Work-around for subpath imports.
+  // See: https://giters.com/facebook/jest/issues/11100
   "moduleNameMapper": {
     "@eslint/eslintrc": "@eslint/eslintrc/dist/eslintrc-universal.cjs"
   },
