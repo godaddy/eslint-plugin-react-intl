d5b9c7f193f00116246808a0f3caf06e07328e61
tgarciaalcala-godaddy <100316982+tgarciaalcala-godaddy@users.noreply.github.com>
2022-04-26 12:24:18 -0400

    feat: some changes for testing cases (#14)
    * feat: some changes for testing cases
    
    * feat: remove unnecessary depency
    
    * feat: some comments added

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  collectCoverageFrom: ['lib/**/*.js']
+  collectCoverageFrom: ['lib/**/*.js'],
+  // Work-around for subpath imports.
+  // See: https://giters.com/facebook/jest/issues/11100
+  "moduleNameMapper": {
+    "@eslint/eslintrc": "@eslint/eslintrc/dist/eslintrc-universal.cjs"
+  },
 };
