29510d61412fa4af7e6152bc8bcb220b1e4c060c
Michael Mason <mmason2@godaddy.com>
2022-09-14 12:05:34 -0700

    update jest, remove obsolete configuration

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   collectCoverageFrom: ['lib/**/*.js'],
-  // Work-around for subpath imports.
-  // See: https://giters.com/facebook/jest/issues/11100
-  "moduleNameMapper": {
-    "@eslint/eslintrc": "@eslint/eslintrc/dist/eslintrc-universal.cjs"
-  },
 };
